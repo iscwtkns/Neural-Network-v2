still cant get proper nonlinearity working in my predictions, not sure whats wrong because gradient calculation appears to be done correctly. Might need to add momentum to the gradient descent.

layer.py
@@ -13,6 +13,11 @@ class Layer:
         self.neurons = [neuron.Neuron(int(n_inputs), activation_function=activation_function) for i in range(n_neurons)]
         self.activation_function = activation_function
         self.assign_weights()
+    def get_error_vector(self):
+        error_vector = []
+        for neuron in self.neurons:
+            error_vector.append(neuron.error_term)
+
     def forward(self,inputs):
         '''
         Uses forward function in neuron class on all neurons inside the layer, stores the outputs as a separate variable
@@ -47,7 +52,7 @@ class Layer:
             std_dev = np.sqrt(2/self.n_inputs)
             weight_matrix = np.random.normal(0,std_dev,(len(self.neurons),self.n_inputs))
         else:
-            weight_matrix = np.random.random((len(self.neurons),self.n_inputs))
+            weight_matrix = np.random.normal(0,1,(len(self.neurons),self.n_inputs))
         for i in range(len(weight_matrix)):
             self.neurons[i].weights = weight_matrix[i]
 

main.py
@@ -3,29 +3,55 @@ import network, math_utils as mu, matplotlib.pyplot as plt, numpy as np
 
 # Create a simple neural network
 neural_net = network.Network(1)
+neural_net.addLayer(2, activation_function=mu.activation_function.leakyRelu)
 neural_net.addLayer(5, activation_function=mu.activation_function.leakyRelu)
-neural_net.addLayer(5, activation_function=mu.activation_function.leakyRelu)
-neural_net.addLayer(5, activation_function=mu.activation_function.leakyRelu)
-neural_net.addLayer(1, activation_function=mu.activation_function.leakyRelu)
+neural_net.addLayer(2, activation_function=mu.activation_function.leakyRelu)
+neural_net.addLayer(1)
 
-inputX = [x for x in range(0,1000)]
+
+inputX = [x for x in range(1000)]
 inputY = [x**2 for x in inputX]
-scaleFactor = np.maximum(np.max(inputX),np.max(inputY))
-inputX = inputX/scaleFactor
-inputY = inputY/scaleFactor
-for i in range(100):
-    randomIndices = [int(np.floor(np.random.random()*1000)) for j in range(16)]
-    randomX = [0]*16
-    randomY = [0]*16
-    for j in range(16):
-        randomX[j] = inputX[randomIndices[j]]
-        randomY[j] = inputY[randomIndices[j]]
-    neural_net.train(randomX, randomY, 0.001,1, announcer=True)
+scaleFactorX = np.max(inputX)
+scaleFactorY = np.max(inputY)
+inputX = inputX/scaleFactorX
+inputY = inputY/scaleFactorY
+random = int(np.floor(np.random.random()*1000))
+randomInput = inputX[random]
+neural_net.learn(randomInput,inputY[random])
+print("Random Input was:", randomInput)
+for layer in neural_net.layers:
+    print("New layer")
+    for neuron in layer.neurons:
+        print("New neuron")
+        print("Input:", neuron.inputs)
+        print("Error Term:", neuron.error_term)
+        print("Weight values:", neuron.weights)
+        print("Bias value:", neuron.bias)
+        print("Weighted Output:", neuron.weighted_output)
+        print("activation:", neuron.activation)
+for value in neural_net.outputs:
+    error = (value-inputY[random])**2
+print("Error:", error, "Cost:", mu.data_function.cost(neural_net.outputs[0],inputY[random]))
 
+
+neural_net.train(inputX, inputY, 0.01,1000, announcer=True,learn_decay=1)
+for layer in neural_net.layers:
+    print("New layer")
+    for neuron in layer.neurons:
+        print("New neuron")
+        print("Input:", neuron.inputs)
+        print("Error Term:", neuron.error_term)
+        print("Weight values:", neuron.weights)
+        print("Bias value:", neuron.bias)
+        print("Weighted Output:", neuron.weighted_output)
+        print("activation:", neuron.activation)
+        print("Bias Derivative:", neuron.bias_derivatives)
+        print("Weight Derivatives:",neuron.weight_derivatives)
 neural_net.calculateNumericalDerivatives(inputX, inputY)
 predictions = neural_net.predict(inputX)
-actual_values = inputY
-input_values = inputX
+predictions = [predictions[i]*scaleFactorY for i in range(len(predictions))]
+actual_values = inputY*scaleFactorY
+input_values = inputX*scaleFactorX
 plt.scatter(input_values,actual_values)
 plt.plot(input_values,predictions)
 plt.show()

network.py
@@ -45,7 +45,7 @@ class Network:
                 output[i * len(self.outputs) + j] = self.outputs[j]
         return output
     
-    def learn(self, inputs, desired_output, learn_step):
+    def learn(self, inputs, desired_output):
         '''
         Takes in inputs that match length of input neurons and output matching length of output neurons.
         Computes the error term for each neuron and then stores derivatives in array for each neuron
@@ -69,6 +69,7 @@ class Network:
         '''
     
         inputs = [inputs[i*self.n_inputs:(i+1)*self.n_inputs][0] for i in range(len(inputs)//self.n_inputs)]
+        
         outputs = [desired_outputs[i*self.n_inputs:(i+1)*self.n_inputs][0] for i in range(len(inputs)//self.n_inputs)]
         for i in range(epochs):
             for layer in self.layers:
@@ -77,11 +78,12 @@ class Network:
                     neuron.bias_derivatives = []   
             for j in range(len(inputs)):
                 #Sets up large gradient arrays that can be averaged to find cost gradients
-                self.learn(inputs[j],outputs[j],learn_step)
+                self.learn(inputs[j],outputs[j])
             for layer in self.layers:
                 for neuron in layer.neurons:
                     neuron.weight_derivatives =[(np.mean(neuron.weight_derivatives[i])) for i in range(len(neuron.weights))]
                     neuron.bias_derivatives = np.mean(neuron.bias_derivatives)
+                    neuron.error_term = neuron.bias_derivatives
                 layer.adjust_weights_and_biases(learn_step)
             predictions = self.massForward(inputs)
             cost = mu.data_function.cost(predictions, inputs)
@@ -126,6 +128,7 @@ class Network:
                 output_error_vector.append(mu.data_function.costDerivative(neuron.activation,desired_output)*neuron.activation_derivative(neuron.weighted_output))
             else:
                 output_error_vector.append(mu.data_function.costDerivative(neuron.activation,desired_output[i])*neuron.activation_derivative(neuron.weighted_output))
+        
         return output_error_vector
     
     